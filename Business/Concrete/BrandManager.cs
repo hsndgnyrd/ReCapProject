4423cbe873f61b6f122f8c9f529181c03df74e4f
hsndgnyrd <hasandugunyurdu27@gmail.com>
2023-11-27 16:01:29 +0300

    Managerlara validationlar eklendi.

--- Business/Concrete/BrandManager.cs
+++ Business/Concrete/BrandManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntitiyFramework;
@@ -20,12 +22,10 @@ namespace Business.Concrete
             _brandDal = brandDal;
         }
 
+        [ValidationAspect(typeof(BrandValidator))]
         public IResult Add(Brand brand)
         {
-            if(brand.BrandName.Length < 2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+            
             _brandDal.Add(brand);
             return new SuccessResult(Messages.ProductAdded);
         }
--- Business/Concrete/ColorManager.cs
+++ Business/Concrete/ColorManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntitiyFramework;
@@ -20,12 +22,9 @@ namespace Business.Concrete
             _colorDal = colorDal;
         }
 
+        [ValidationAspect(typeof(ColorValidator))]
         public IResult Add(Color color)
         {
-            if (color.ColorName.Length < 2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
             _colorDal.Add(color);
             return new SuccessResult(Messages.ProductAdded);
         }
